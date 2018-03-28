468e42ae1bbd8ce34ad4be401d4ecd6cfc857060
chris <dwatow@gmail.com>
2018-03-28 10:21:17 +0800

    add Readme.md

--- 2018-03-27/Chris-plugin/src/loader.js
+++
@@ -1,8 +0,0 @@
-const loaderUtils = require('loader-utils');
-
-module.exports = function loader(source) {
-  //this is options of loader module in webpack.config.js
-  const options = loaderUtils.getOptions(this);
-
-  return `module.exports = 'I am ${options}'`;
-};
