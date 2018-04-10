d864fdd80fbe5015e01d7ef1ff2980d5f31d49cc
chris <dwatow@gmail.com>
2018-04-10 11:34:03 +0800

    add vue-chart

---
+++ 2018-04-10/Chris/src/components/chart.vue
@@ -0,0 +1,22 @@
+
+<script>
+import { Bar } from 'vue-chartjs'
+
+export default {
+  props: ["labels", "data"],
+  extends: Bar,
+  mounted () {
+    // Overwriting base render method with actual data.
+    this.renderChart({
+      labels: this.labels,
+      datasets: [
+        {
+          label: 'GitHub Commits',
+          backgroundColor: '#f87979',
+          data: this.data
+        }
+      ]
+    })
+  }
+}
+</script>
