c3e999e3878cffa00357b09d32622a2448700abd
chris <dwatow@gmail.com>
2018-04-16 18:40:30 +0800

    add my file

---
+++ 2018-04-17/Chris/README.md
@@ -0,0 +1,9 @@
+# vue-webpack-demo
+
+練習 vue 的 webpack 樣版，做簡單切版
+
+## 練習內容
+
+- vue-slot
+- dynamic component
+- lifecycle
---
+++ 2018-04-17/Chris/src/components/features.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <div class="">
+    <h1>This is Features</h1>
+    <p>nothoing here...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  created () {
+    console.log('created')
+  },
+  destroyed () {
+    console.log('destoryed')
+  },
+  deactivated () {
+    console.log('deactivated')
+  },
+  activated () {
+    console.log('activated')
+  }
+}
+</script>
+
+<style lang="css">
+</style>
